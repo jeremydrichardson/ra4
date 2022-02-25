3be3d438f81b6fbed0a3363189f299ea7f4a1181
Jeremy Richardson <jrichardson@thought-dev.com>
2022-02-25 13:43:14 -0600

    feat: basic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// in src/App.js
+import * as React from "react";
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  CustomRoutes,
+  useLogout,
+  useAuthenticated,
+} from "react-admin";
+import { Route } from "react-router-dom";
+import jsonServerProvider from "ra-data-json-server";
+import authProvider from "./authProvider";
 
-function App() {
+const LoggedOutPage = () => {
+  console.log("logged out page");
+  return <p>You have been logged out.</p>;
+};
+
+const Page = () => {
+  useAuthenticated();
+  const logout = useLogout();
+  const handleClick = () => {
+    logout({}, "/loggedout");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      This is a page <button onClick={handleClick}>Logout</button>
+    </>
   );
-}
+};
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => (
+  <Admin
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    dashboard={Page}
+  >
+    <Resource name="users" list={ListGuesser} />
+    <CustomRoutes noLayout>
+      <Route path="/loggedout" element={<LoggedOutPage />} />
+      <Route path="/page" element={<Page />} />
+    </CustomRoutes>
+  </Admin>
+);
 
 export default App;
